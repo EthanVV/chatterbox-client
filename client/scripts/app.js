0692f3c080cc9b7c1757edcdb494ea525761d6b7
Hack Reactor Student <student@hackreactor.com>
2018-09-29 17:16:58 -0500

    Added fetch messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,15 +1,22 @@
 //YOUR CODE HERE:
-class App {
-  
-  constructor () {
-    this.server = 'http://parse.atx.hackreactor.com/chatterbox/classes/messages';
-  }
-  
-  init() {
-    this.fetch();
+var app = {
+    server : 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+    username: 'anonymous',
+    roomname: 'lobby'
+}
+
+
+  app.init = () => {
+    app.$message = $('#message');
+    app.$chats = $('#chats');
+    app.$roomSelect = $('#roomSelect');
+    app.$send = $('#send');
+    app.lastMessageId = 0;
+
+    app.fetch();
   }
 
-  send(message) {
+  app.send = (message) => {
     console.log('sending...')
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
@@ -27,17 +34,32 @@ class App {
     });
   }
 
-  fetch() {
+  app.fetch = () => {
     console.log('fetchin...');
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       // url:  app.server,
-      url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',   
+      url: app.server,   
       type: 'GET',
       contentType: 'application/json',
       // data: null,
       success: function (data) {
         console.log('chatterbox: Message received', data);
+        if (!data.results || !data.results.length) { return;}
+        app.messages = data.results;
+
+        var mostRecentMessage = app.messages[app.messages.length - 1];
+        if(mostRecentMessage.objectID !== app.lastMessageId) {
+          app.$chats.html('');
+          for( var i=0;  i<app.messages.length; i++) {
+            var $chat = $('<div class="chat"/>');
+            var $username = $('<span class ="username">' + app.messages[i].text + '</span>');
+            $username.appendTo();
+            var $message = $('<br><span>' + app.messages[i].text + '</span>');
+            $message.appendTo($chat);
+            app.$chats.append($chat);
+          }
+        }
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -47,17 +69,18 @@ class App {
   }
 
 
-  clearMessages() {
+  app.clearMessages = () => {
     $('#chats').empty();
   }
 
-  handleUsernameClick() {
+  app.handleUsernameClick = () => {
   }
 
-  handleSubmit() {
+  app.handleSubmit = () => {
+    console.log('submit reached');
   }
 
-  renderMessage(message) {
+  app.renderMessage = (message) => {
     $('#chats').append('<div class = "chat username"> ' 
       + '<div class="username" onclick="app.handleUsernameClick()">' + message.username + '</div>'
       + '<div>' + message.text + '</div>'
@@ -65,9 +88,7 @@ class App {
       + '</div>');
   }
 
-  renderRoom(room) {
+  app.renderRoom = (room) => {
     $('#roomSelect').append('<p>' + room + '</p>');
   }
-}
 
-var app = new App();
