4e2bff4face7de4baf273141bca21e86e4e2cc3e
Hack Reactor Student <student@hackreactor.com>
2018-09-28 19:23:09 -0500

    complete send functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,22 +3,20 @@ let app = {
   init: function() {
   
   },
-  send: function() {
-      
+  send: function(message) {
+      $.ajax({
+          // This is the url you should use to communicate with the parse API server.
+          url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+          type: 'POST',
+          data: JSON.stringify(message),
+          contentType: 'application/json',
+          success: function (data) {
+          console.log('chatterbox: Message sent');
+          },
+          error: function (data) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to send message', data);
+          }
+      });
   }
 };
-
-// $.ajax({
-//     // This is the url you should use to communicate with the parse API server.
-//     url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
-//     type: 'POST',
-//     data: JSON.stringify(message),
-//     contentType: 'application/json',
-//     success: function (data) {
-//     console.log('chatterbox: Message sent');
-//     },
-//     error: function (data) {
-//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message', data);
-//     }
-// });
